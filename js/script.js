716f67e8501f29e36982df7c5f5d5989b55f6ac8
Patrick Kelly <kell2456@umn.edu>
2022-09-28 23:44:48 -0500

    push

--- js/script.js
+++ js/script.js
@@ -1,14 +1,56 @@
 
-let lat = 39.2904
-let lon = 76.6122
+
+
+let lat = 0;
+let lon = 0;
+
+var Srchbtn = document.getElementById('srchbtn');
+
+
+var weather = [
+    { temperature : '', Wind : '' , Humidity : '' },
+    { temperature : '', Wind : '' , Humidity : '' },
+    { temperature : '', Wind : '' , Humidity : '' },
+    { temperature : '', Wind : '' , Humidity : '' },
+    { temperature : '', Wind : '' , Humidity : '' },
+];
+
+
+function getCity () {
+    let city = document.getElementById('whatcity').value;
+
+    console.log(whatcity.value);
+
+    console.log(city);
+    
+    var requestUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=6cfdbf06909e62bf85aa67f8d761af43`;
+
+    fetch(requestUrl)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+        lat = data[0].lat;
+        lon = data[0].lon;
+        console.log(lat);
+        console.log(lon);
+        getWeather();
+    });
+}
 
 
 
-var Srchbtn = document.getElementById('searchinfo');
+
+
+
+
+
+
 
 
 function getWeather () {
-    var requestUrl = `https://api.openweathermap.org/data/2.5/forecast?lat={${lat}}&lon={${lon}}&appid={6cfdbf06909e62bf85aa67f8d761af43}`;
+    var requestUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=6cfdbf06909e62bf85aa67f8d761af43`;
 
     fetch(requestUrl)
     .then(function (response) {
@@ -17,9 +59,21 @@ function getWeather () {
     .then(function (data) {
         // Use the console to examine the response
         console.log(data);
-        console.log(data.results);
 
-});
+        for( i = 0; i < weather.length; i++) {
+            weather[i].temperature = data.list[0].main.temp;
+        }
+
+        console.log(weather)
+
+        // const weatherTxt = document.createElement("p");
+
+        // const weatherNew = document.te(weather);
+
+        // weatherTxt.appendChild(weatherNew);
+
+
+    });
 }
 
 
@@ -39,4 +93,4 @@ function getWeather () {
 
 
 
-Srchbtn.addEventListener('click', getWeather);
+Srchbtn.addEventListener('click', getCity);
